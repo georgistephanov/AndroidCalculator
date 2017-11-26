13a745f5ce920003668e4b6cca252f23361073f3
Georgi Stefanov <georgistephanov@gmail.com>
2017-11-26 21:51:30 +0000

    Started implementing the user interaction logic. Added onClick listeners for the digit buttons, the clear button and the point button.

--- app/src/main/java/com/georgistephanov/android/calculator/MainActivity.java
+++ app/src/main/java/com/georgistephanov/android/calculator/MainActivity.java
@@ -12,12 +12,56 @@ package com.georgistephanov.android.calculator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
+import java.util.Locale;
 
 public class MainActivity extends Activity {
+	private StringBuilder inputNumber;
+	private String inputNumberString;
+	private TextView v_inputNumber;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		inputNumber = new StringBuilder();
+		v_inputNumber = (TextView) findViewById(R.id.input_number);
+	}
+
+	public void onOperationButtonClick(View view) {
+		if ( inputNumber.length() < 12 ||
+				inputNumber.toString().contains(".") && inputNumber.length() < 13) {
+			Button pressedButton = (Button) view;
+			double numberPressed = Double.parseDouble(pressedButton.getText().toString());
+
+			if ( inputNumber.length() == 0 && numberPressed == 0 ) {
+				return;
+			}
+
+			inputNumber.append(pressedButton.getText().toString());
+			v_inputNumber.setText(DecimalFormat.getNumberInstance(Locale.US).format(Double.parseDouble(inputNumber.toString())));
+
+		} else {
+			Toast.makeText(this, R.string.max_input_size_toast_message, Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	public void onClearButtonClick(View view) {
+		if (inputNumber.length() > 0) {
+			inputNumber = new StringBuilder();
+			v_inputNumber.setText("");
+		}
+	}
+
+	public void onPointPressed(View view) {
+		if ( !(inputNumber.toString().contains("."))) {
+			inputNumber.append(".");
+		}
 	}
 }
