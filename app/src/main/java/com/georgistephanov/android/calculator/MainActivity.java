d5c4b71a2e122cf43af208912a5cd02396b1ce79
Georgi Stefanov <georgistephanov@gmail.com>
2017-11-27 11:54:20 +0000

    Implementing the operation and the second number input

--- app/src/main/java/com/georgistephanov/android/calculator/MainActivity.java
+++ app/src/main/java/com/georgistephanov/android/calculator/MainActivity.java
@@ -17,13 +17,16 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.util.Locale;
 
 public class MainActivity extends Activity {
+	private static int MAXIMUM_NUMBER_OF_DIGITS = 15;
 	private StringBuilder inputNumber;
-	private String inputNumberString;
 	private TextView v_inputNumber;
+	private Character operation;
+	//private DecimalFormat formatter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -32,36 +35,54 @@ public class MainActivity extends Activity {
 
 		inputNumber = new StringBuilder();
 		v_inputNumber = (TextView) findViewById(R.id.input_number);
+		v_inputNumber.setText("0");
+
+		/*formatter = (DecimalFormat) DecimalFormat.getNumberInstance(Locale.US);
+		formatter.setMaximumFractionDigits(12);*/
 	}
 
 	public void onOperationButtonClick(View view) {
-		if ( inputNumber.length() < 12 ||
-				inputNumber.toString().contains(".") && inputNumber.length() < 13) {
-			Button pressedButton = (Button) view;
-			double numberPressed = Double.parseDouble(pressedButton.getText().toString());
+		Button pressedButton = (Button) view;
+		double numberPressed = Double.parseDouble(pressedButton.getText().toString());
 
-			if ( inputNumber.length() == 0 && numberPressed == 0 ) {
-				return;
-			}
-
-			inputNumber.append(pressedButton.getText().toString());
-			v_inputNumber.setText(DecimalFormat.getNumberInstance(Locale.US).format(Double.parseDouble(inputNumber.toString())));
-
-		} else {
-			Toast.makeText(this, R.string.max_input_size_toast_message, Toast.LENGTH_SHORT).show();
+		if ( inputNumber.length() == 0 && numberPressed == 0 ) {
+			return;
 		}
+
+		updateInputField(pressedButton.getText().toString());
 	}
 
 	public void onClearButtonClick(View view) {
 		if (inputNumber.length() > 0) {
 			inputNumber = new StringBuilder();
-			v_inputNumber.setText("");
+			v_inputNumber.setText("0");
 		}
 	}
 
-	public void onPointPressed(View view) {
+	public void onPointClick(View view) {
 		if ( !(inputNumber.toString().contains("."))) {
 			inputNumber.append(".");
+			v_inputNumber.setText(inputNumber.toString());
+		}
+	}
+
+	public void onOperationClick(View view) {
+		if (operation == null) {
+			Button operationButton = (Button) view;
+			operation = operationButton.getText().toString().charAt(0);
+		}
+	}
+
+	private void updateInputField(String toAppend) {
+		if ( inputNumber.length() < MAXIMUM_NUMBER_OF_DIGITS ||
+				inputNumber.toString().contains(".") && inputNumber.length() < MAXIMUM_NUMBER_OF_DIGITS + 1 ) {
+
+			inputNumber.append(toAppend);
+			v_inputNumber.setText(inputNumber.toString());
+
+		} else {
+			// Maximum length of the number is reached
+			Toast.makeText(this, R.string.max_input_size_toast_message, Toast.LENGTH_SHORT).show();
 		}
 	}
 }
